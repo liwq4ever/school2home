ae6dbf9ed626944b98542141e72391310d8ecbe4
李文泉 <liwq@192.168.1.105>
2020-10-06 13:35:54 +0800

    Initial commit

---
+++ src/main/java/com/simpletech/school2home/School2homeApplication.java
@@ -0,0 +1,13 @@
+package com.simpletech.school2home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School2homeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(School2homeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simpletech/school2home/School2homeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpletech.school2home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School2homeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
